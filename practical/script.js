a4bf0cb52d08e0cc6eb58ade82c298d747daf288
akanni malik <akannimalik1@gmail.com>
2024-05-20 20:04:02 -0400

    jkm,

--- practical/script.js
+++ practical/script.js
@@ -1,18 +1,27 @@
-function userDetails() {
-  let userOne = {
-    //creating userOne first object
-    name: "Musa",
-    role: "dev",
-    country: "Surulere",
-  };
+// let name = "fuad";
+// console.log(typeof name);
 
-  let userTwo = {
-    //creating UserTwo object
-    name: "Akanni",
-    role: "frontend",
-    country: "gudu",
-  };
+// console.log(name.length);
 
-  return { userOne, userTwo }; // Return an object containing userOne and userTwo details
+// let age = 19;
+
+// // console.log(name == age);
+// // console.log("musthapha" == 20);
+
+// console.log(typeof "malik" == typeof "musthapha");
+
+// let data = {
+//   name: " malik",
+//   surname: "akanni",
+//   cars: ["lambogini", "dodge", 3, " ferarri"],
+//   age: 23,
+// };
+
+// let carTwo = data.cars[2, 3];
+
+// console.log(carTwo);
+
+for (let x = 0; x <= 5; x++) {
+  console.log(x);
 }
-console.log(userDetails());
+

519c15d95875a751e78d82a23175355f919e1908
akanni malik <64205553+tr199er@users.noreply.github.com>
2025-02-17 16:38:27 +0100

    yoo 2/11

--- 04. Conditionals/script.js
+++ 04. Conditionals/script.js
@@ -9,20 +9,20 @@ Conditions can be implementing using the following ways:
 
 //IF
 
-let age = 30;
+// let age = 30;
 
-if (age > 18) {
-  console.log(`${age} is an adult age`);
-}
+// if (age > 18) {
+//   console.log(`${age} is an adult age`);
+// }
 
-//IF ELSE
-let takeAge = 13;
+// //IF ELSE
+// let takeAge = 13;
 
-if (takeAge > 18) {
-  console.log(`${takeAge} is an adult age`);
-} else {
-  console.log(`given ur age of ${takeAge}, you're still a kid`);
-}
+// if (takeAge > 18) {
+//   console.log(`${takeAge} is an adult age`);
+// } else {
+//   console.log(`given ur age of ${takeAge}, you're still a kid`);
+// }
 
 //COMBINATION OF PROMPT, ALERT AND IF/ELSE
 // let guestAge = prompt("how old are you");
@@ -35,43 +35,84 @@ if (takeAge > 18) {
 
 //if else if else
 
-let a;
+// let a;
 
-if (a > 1) {
-  console.log(`${a} is greater than 1`);
-} else if (a == 1) {
-  console.log(`${a} is one`);
-} else if (a < 1) {
-  console.log(`${a} is zero or a negative number `);
-} else {
-  console.log(`${a} is not a number`);
-}
+// if (a > 1) {
+//   console.log(`${a} is greater than 1`);
+// } else if (a == 1) {
+//   console.log(`${a} is one`);
+// } else if (a < 1) {
+//   console.log(`${a} is zero or a negative number `);
+// } else {
+//   console.log(`${a} is not a number`);
+// }
 
 //SWITCH
-let weekDay = "friday";
+// let weekDay = "friday";
 
-switch (weekDay) {
-  case "monday":
-    console.log("it the beginning of a new week");
-    break;
-  case "wednesday":
-    console.log("it the middle of the week");
-    break;
-  case "friday":
-    console.log("weekend loading...TGIF");
-    break;
-  case "saturday":
-    console.log("weekend...");
-    break;
-  case "sunday":
-    console.log("sabatth day...");
-    break;
-  default:
-    console.log("normal day");
-    break;
-}
+// switch (weekDay) {
+//   case "monday":
+//     console.log("it the beginning of a new week");
+//     break;
+//   case "wednesday":
+//     console.log("it the middle of the week");
+//     break;
+//   case "friday":
+//     console.log("weekend loading...TGIF");
+//     break;
+//   case "saturday":
+//     console.log("weekend...");
+//     break;
+//   case "sunday":
+//     console.log("sabatth day...");
+//     break;
+//   default:
+//     console.log("normal day");
+//     break;
+// }
 
 //TERNARY OPERATION
-let hayatu = 18;
+// let hayatu = 18;
 
-hayatu == 17 ? console.log("hello") : console.log("hi");
+// hayatu == 17 ? console.log("hello") : console.log("hi");
+
+
+// let malik ;
+// malik = 26;
+
+// if (malik == 26) {
+//   console.log("A youth");
+// } else {
+//   console.log("hmmm");
+// }
+
+
+// let babyAge = prompt="what ur baby age?";
+
+// switch (babyAge) {
+//   case 1:
+//     console.log("a kid ");
+//     break;
+
+//   case babyAge < 1:
+//     console.log("infant");
+    
+
+//   default:
+//     console.log("not more a baby");
+    
+//     break;
+// }
+
+
+//EXERCISE
+let userAge = 18;
+let underAge = 18 - userAge;
+
+if (userAge > 18 || userAge == 18){
+  console.log("you're old enough");
+  
+}else {
+  console.log(`wait for ${underAge} years`);
+  
+}
